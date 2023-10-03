52865b7ca2f8413063d8aae8a04aa3bc7d005341
Tunzale1 <tunzaleimanova03@mail.ru>
2023-10-03 20:08:37 +0400

    m

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-import express from "express"
 
-const pets=[
-    {id:1,
-    "name":"mestan"},
-    {
-    id:2,
-    "name":"toplan",
-    }
-]
-const app=express()
-app.get ("/pets", (req, res)=>{
- 
-})
-app.get("/pets/:id" ,(req, res)=>{
-    return res.json(pets.find(({id})=> id ===+req.params.id))
-})
-app.listen (10000,()=>{
-    console.log('salam')
-})
+import express from "express";
+
+const pets = [
+  { id: 1, name: "mestan" },
+  { id: 2, name: "toplan" },
+];
+
+const app = express();
+app.use(express.json()); // Add JSON parsing middleware
+
+app.get("/pets", (req, res) => {
+  return res.json(pets);
+});
+
+app.get("/pets/:id", (req, res) => {
+  const pet = pets.find(({ id }) => id === +req.params.id);
+  if (!pet) {
+    return res.status(404).json({ message: "Pet not found" });
+  }
+  return res.json(pet);
+});
+
+app.listen(1000, () => {
+  console.log('Server is running on port 10000');
+});
